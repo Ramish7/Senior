06a086c9925e524beceedc49fb7095415b520199
Ramish Saeed <ramish.saeed10@gmail.com>
2019-04-21 14:00:19 +0300

    test commit

---
+++ app/src/main/java/com/info/eventoutfyp/VenueOwner.java
@@ -0,0 +1,13 @@
+package com.info.eventoutfyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VenueOwner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_owner);
+    }
+}
